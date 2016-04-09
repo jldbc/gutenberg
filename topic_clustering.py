fec07da6e029d1fb039b84fbe228cf3bdb947d50
James LeDoux <ledouxja@bc.edu>
2016-04-09 19:48:44 -0400

    functioning k means

--- topic_clustering.py
+++ topic_clustering.py
@@ -2,6 +2,10 @@ from pyspark.mllib.clustering import KMeans, KMeansModel
 from pyspark.mllib.linalg import SparseVector
 from pyspark import SparkConf, SparkContext
 from os.path import join
+from pyspark.mllib.util import MLUtils
+from pyspark.mllib.feature import StandardScaler
+from pyspark.mllib.linalg import Vectors
+import csv
 import sys
 
 
@@ -13,16 +17,16 @@ uid, movid, rating, timestamp
 
 make a matrix:    cols words, rows Books
 
-
+to run:
 /Users/jamesledoux/spark-1.6.1/bin/spark-submit  /Users/jamesledoux/Documents/BigData/gutenberg/topic_clustering.py  /Users/jamesledoux/Documents/BigData/gutenberg/
+to open the pyspark console
 /Users/jamesledoux/spark-1.6.1/bin/pyspark
 
 """
 if(len(sys.argv) != 2):
-    print "usage: /sparkPath/bin/spark-submit  name.py  movieDirectory"
+    print "usage: /sparkPath/bin/spark-submit  name.py  wordDirectory"
 
-conf = SparkConf().setAppName("KMeans TF-IDF").set("spark.executor.memory", "15g")
-#sc = SparkContext(conf)
+conf = SparkConf().setAppName("KMeans TF-IDF").set("spark.executor.memory", "7g")
 HomeDir = sys.argv[1]   # passed as argument
 #HomeDir = "/Users/jamesledoux/Documents/Big\ Data/movielens/medium/"
 #HomeDir = "/Users/jamesledoux/Documents/BigData/gutenberg/"
@@ -48,76 +52,47 @@ print "sample rating: ", rows.take(1)
 
 #rows RDD:  (time stamp, (uid, mid, rows) )
 #num of Books  (userid, wordID, rows)
-numBooks = rows.values().map(lambda x: x[0]).max()+1   #num books
-numWords = rows.values().map(lambda x: x[1]).max()+1  #num words
+numBooks = rows.values().map(lambda x: x[0]).max()+1
+numWords = rows.values().map(lambda x: x[1]).max()+1
 
 # ntransform into sparse vectors
 """
 represent as sparse vector to save on space
 
-three cols: id, vector size, movie:rating dictionary
-size needed so u know how many zeros are in the mat?
-uid, size of vector *around 3,000), (movie:rating, movie:rating, movie:rating))
+three cols: id, vector size, wordId:TfIdfscore dictionary
+size needed so u know how many zeros are in the sparse matrix
+uid, size of vector, (wordid:score, wordid:score, wordid:score))
 """
 
 
 wordsSparseVector = vectorize( rows.values(), numWords)
-print "wordsSparseVector Type:", type(wordsSparseVector)
-print "wordsSparseVector Count:", wordsSparseVector.count()
+#scaler = StandardScaler(withMean = False, withStd = True).fit(features)  #becomes dense if using withMean. may run out of memory locally
 
 
-#next: include a validation set. train on train, get errors on validation
-#then: score == RMSE on the test set
-minError = float("inf")
-bestModel = None
-bestK = None
-test_values = [2]
-error_storage = []
-for i in test_values:
-    model = KMeans.train(wordsSparseVector.values(), i, maxIterations = 20, runs = 10)
-    #test this error on the validation set once we make that
-    error = model.computeCost(wordsSparseVector.values())
-    error_storage.append(error)
-    if error < minError:
-        bestModel = model
-        minError = error
-        bestK = i
+#train the model
+#note: we really have no idea what errors would be with this kind of data.
+#Cross validation wouldn't seem to make much sense here. Maybe update this later if we get collaborative data.
+k = 25
+print "training model with " + str(k) + " clusters. . ."
+model = KMeans.train(wordsSparseVector.values(), k, maxIterations = 20, runs = 10)
 
 """
-#save model once you have the best version
-#model.save(sc, "KMeansModelCollaborative")
-#model = KMeansModel.load(sc, "KMeansModelCollaborative")
-
-#take one sample   the [0] is because this will be returned as a list -- not an index
-user = wordsSparseVector(1)[0] #take a sample of 1 from the data set (use test data when doing this)
-label = model.predict(user)   #outputs which cluster this user belongs to
-# ==> a cluster id between 1 and k
-clusterCenters = model.clusterCenters     #a list of centers (len == k)
-clusterCenters[0] #len == total num of words, each obs == avg rating for people in this group
-wordID = 4
-print "predicted value: ", clusterCenters[label][wordID]
+for some reason this goes for all even ids then for all odds.. Should we sort this before writing
+to a csv, or is it fine to leave it unordered?
 """
+#book = wordsSparseVector.values().take(1)[0] #take a sample of 1 from the data set (use test data when doing this)
+books = wordsSparseVector.collect() #book[i][0] == id, book[i][1] == sparse matrix of words:scores
+clusterDict = {}
+for i in books:
+    label = model.predict(i[1])
+    clusterDict[i[0]] = label
+    print "book id " + str(i[0]) + " predicted as a member of cluster "  + str(label)
 
 
 
-
-#for i in range(10, 20):
-# wordsSparseVector = > RDD where each item is ( userID , SparseVector)
-
-##    if error < minError:
-##        bestModel = model
-##        minError = error
-##        bestK = i
-book = wordsSparseVector.values().take(1)[0] #take a sample of 1 from the data set (use test data when doing this)
-print "Type book:", type(book)
-print "Book id:", book
-
-label = model.predict(book)   #outputs which cluster this user belongs to
-clusterCenters = model.clusterCenters     #a list of centers (len == k)
-#clusterCenters[0] #len == total num of words, each obs == avg rating for people in this group
-print "Len Cluster Centers:", len(clusterCenters)
-wordID = 4
-
-print "predicted value: ", clusterCenters[label][wordID]   #movie id == word id, right? this should be book id though so let's figure this out
+#probably should make the write destination a command line argument
+writer2 = csv.writer(open('clusterMembership.csv', 'wb'))
+for key, value in clusterDict.items():
+   writer2.writerow([key, value])
 
 sc.stop()

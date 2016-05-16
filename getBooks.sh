bc4ef2a3f5051d70131c38e63110f99b22718c53
James LeDoux <ledouxja@bc.edu>
2016-05-16 08:47:58 -0400

    shell script for pulling the data

---
+++ getBooks.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+caffeinate -i -m wget -w 2 -m -H "http://www.gutenberg.org/robot/harvest?filetypes[]=txt&langs[]=en <http://www.gutenberg.org/robot/harvest?filetypes%5B%5D=txt&langs%5B%5D=en>”
